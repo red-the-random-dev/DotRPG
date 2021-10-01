dd7c4b3446667f8d22832d274853fde593fcd38b
red-the-random-dev <mrdaniel422@gmail.com>
2021-10-01 18:06:07 +0300

    A small script update

--- _Example/GameData/Scripts/dialog.lua
+++ _Example/GameData/Scripts/dialog.lua
@@ -5,8 +5,10 @@ event_count = 2
 function loop(eventID, frameTime, totalTime)
     if eventID == 0 then
         dialog.Text = "You ran across white rectangular object.\nSo what?"
-        scene.ExitDialog = true
     else if eventID == 1 then
+        dialog.Text = "You should better go elsewhere."
+        scene.ExitDialog = true
+    else if eventID == 2 then
         dialog.Text = "A white rectangular object."
         scene.ExitDialog = true
     end
