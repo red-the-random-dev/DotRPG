e0001a8d65885d98b3de12fe831a60d6e4005622
red-the-random-dev <mrdaniel422@gmail.com>
2021-10-23 10:05:27 +0300

    [w4d7] Implementation of Camera Manager, which will be callable from within the scripts soon

---
+++ Behavior/Management/CameraManager.cs
@@ -0,0 +1,47 @@
+﻿using DotRPG.Objects;
+using DotRPG.Objects.Dynamics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Management
+{
+    public class CameraManager
+    {
+        public CameraFrameObject ManagedCamera;
+        public Dictionary<String, DynamicRectObject> ObjectHeap;
+        public PlayerObject Player;
+
+        protected DynamicRectObject TrackedObject;
+        protected Point Offset = Point.Zero;
+
+        public CameraManager(CameraFrameObject camera, Dictionary<String, DynamicRectObject> objectHeap)
+        {
+            ManagedCamera = camera;
+            ObjectHeap = objectHeap;
+        }
+
+        public void TrackToPlayer()
+        {
+            TrackedObject = Player;
+        }
+
+        public void TrackTo(String ID)
+        {
+            TrackedObject = ObjectHeap[ID];
+        }
+
+        public void SetOffset(Int32 x, Int32 y)
+        {
+            Offset = new Point(x, y);
+        }
+
+        public Point TrackPoint
+        {
+            get
+            {
+                return TrackedObject.Location.ToPoint() + Offset;
+            }
+        }
+    }
+}
