1a9a21307412db06d80d147720ed804805f049c7
red-the-random-dev <mrdaniel422@gmail.com>
2021-10-24 09:56:45 +0300

    [w5d1] Doppler shift implementation in Object Heap Manager

--- Behavior/Management/ObjectHeapManager.cs
+++ Behavior/Management/ObjectHeapManager.cs
@@ -8,12 +8,26 @@ namespace DotRPG.Behavior.Management
 {
     public class ObjectHeapManager
     {
+        protected Single VectorAngleCosine(Vector2 _1, Vector2 _2)
+        {
+            return (_1.X * _2.X + _1.Y * _2.Y) / (_1.Length() * _2.Length());
+        }
+
+        public Single VectorAngleCosine(Single _1x, Single _1y, Single _2x, Single _2y)
+        {
+            return VectorAngleCosine(new Vector2(_1x, _1y), new Vector2(_2x, _2y));
+        }
         protected Dictionary<String, DynamicRectObject> ObjectHeap;
         public PlayerObject Player;
 
         public Single GetScalarVelocity(String name)
         {
             return ObjectHeap[name].Velocity.Length();
+        }
+
+        public Single GetPlayerScalarVelocity()
+        {
+            return Player.Velocity.Length();
         }
 
         public Single GetDistance(String one, String other)
@@ -61,6 +75,43 @@ namespace DotRPG.Behavior.Management
             Vector2 v = ObjectHeap[target].Location - Player.Location;
             v /= v.Length();
             return v.X;
+        }
+
+        public Single GetRelativeScalarVelocity(String center, String other)
+        {
+            return (ObjectHeap[other].Velocity - ObjectHeap[center].Velocity).Length();
+        }
+
+        public Single GetRelativeScalarVelocityFromPlayer(String other)
+        {
+            return (ObjectHeap[other].Velocity - Player.Velocity).Length();
+        }
+
+        public Single GetApproachScalarVelocity(String center, String other)
+        {
+            Vector2 SightLine = ObjectHeap[other].Location - ObjectHeap[center].Location;
+            Single V1 = GetScalarVelocity(other) * VectorAngleCosine(ObjectHeap[other].Velocity, SightLine);
+            Single V2 = GetScalarVelocity(center) * VectorAngleCosine(ObjectHeap[center].Velocity, -SightLine);
+            return V1 + V2;
+        }
+
+        public Single GetApproachScalarVelocityFromPlayer(String other)
+        {
+            Vector2 SightLine = ObjectHeap[other].Location - Player.Location;
+            Single V1 = GetScalarVelocity(other) * VectorAngleCosine(ObjectHeap[other].Velocity, SightLine);
+            Single V2 = GetPlayerScalarVelocity() * VectorAngleCosine(Player.Velocity, -SightLine);
+            return V1 + V2;
+        }
+        /// <summary>
+        /// Gets pitch shift which depends on approach velocity (https://en.wikipedia.org/wiki/Doppler_effect)
+        /// </summary>
+        /// <param name="target">Emitter object</param>
+        /// <param name="sonicSpeed">Sound propagation speed. Default: 12160 dots per second (equivalent to 380 m/s)</param>
+        /// <returns></returns>
+        public Single GetDopplerShift(String target, Single sonicSpeed = 12160.0f)
+        {
+            // f = (1 + deltaV/c) * f0, where deltaV is approach velocity, c is sonic speed and f0 equals 1
+            return 1.0f + (GetApproachScalarVelocityFromPlayer(target) / sonicSpeed);
         }
     }
 }
