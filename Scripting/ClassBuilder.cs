fe0d52290e1547b2122e274d0773caeaa8194fe2
red-the-random-dev <mrdaniel422@gmail.com>
2021-10-24 22:03:00 +0300

    Revert "Revert "[w5d1] Implementation of run-time script module compilation""
    This reverts commit 621282659ebce7bb6401cc552902c9eb397fda2e.

---
+++ Scripting/ClassBuilder.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotRPG.Scripting
+{
+    public static class ClassBuilder
+    {
+        public static CSIModule ComplileFromFile(String fullPath, List<String> CompilerLog = null)
+        {
+            Assembly[] ass = AppDomain.CurrentDomain.GetAssemblies();
+            List<String> names = new List<string>();
+            foreach (Assembly a in ass)
+            {
+                names.Add(a.FullName);
+            }
+            CompilerParameters cp = new CompilerParameters(names.ToArray());
+            cp.GenerateExecutable = false;
+            cp.GenerateInMemory = true;
+            CodeDomProvider provider = CodeDomProvider.CreateProvider("CSharp");
+            var results = provider.CompileAssemblyFromFile(cp, new string[]{fullPath});
+            if (results.Errors.HasErrors)
+            {
+                if (CompilerLog != null)
+                {
+                    foreach (CompilerError ce in results.Errors)
+                    {
+                        CompilerLog.Add(ce.ErrorNumber + ": " + ce.ErrorText);
+                    }
+                }
+                return null;
+            }
+            else
+            {
+                foreach (Type x in results.CompiledAssembly.GetTypes())
+                {
+                    if (typeof(CSIModule).IsAssignableFrom(x))
+                    {
+                        return (CSIModule)Activator.CreateInstance(x);
+                    }
+                }
+                return null;
+            }
+        }
+    }
+}
